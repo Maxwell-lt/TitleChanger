f124bca033101c9ff79f97537456ae8be6ecd6ff
Maxwell Lanzkron-Tamarazo <maxwell.lt@live.com>
2017-01-16 15:01:41 -0500

    Initial Commit

---
+++ src/main/java/maxwell_lt/titlechanger/TitleChanger.java
@@ -0,0 +1,18 @@
+package maxwell_lt.titlechanger;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = TitleChanger.MODID, version = TitleChanger.VERSION)
+public class TitleChanger
+{
+    public static final String MODID = "titlechanger";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        
+    }
+}
